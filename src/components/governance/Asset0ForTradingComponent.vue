99875bb3a9aadc9436a26c07f2cb82ace0d65741
Evgenii Stulnikov <xxjenekxx@gmail.com>
2023-10-28 20:48:20 +0300

    Changed Governance asset in Trade

--- src/components/governance/Asset0ForTradingComponent.vue
+++ src/components/governance/Asset0ForTradingComponent.vue
@@ -1,8 +1,6 @@
 <script setup>
 import { onMounted, ref } from "vue";
-import { fullExplorerUrlForAddress } from "@/config";
 import { getPriceByAssets } from "@/services/PerpAPI";
-import TooltipComponent from "@/components/TooltipComponent.vue";
 
 const props = defineProps([
   "aa",
@@ -37,30 +35,9 @@ onMounted(async () => {
         Status:
         <span class="font-light text-sm"> trading </span>
       </div>
-      <div
-        class="card-actions sm:justify-start block sm:flex text-center sm:text-left"
-      >
-        <div class="divider w-full"></div>
-        <div class="text-sm w-full">
-          <div class="font-bold text-lg flex items-center">
-            Price aa
-            <TooltipComponent class="inline-block ml-1" field-name="price_aa">
-            </TooltipComponent>
-          </div>
-          <div class="mt-2">
-            Current value:
-            <a
-              class="link text-sky-500 link-hover block sm:inline-block text-xs sm:text-sm"
-              target="_blank"
-              :href="fullExplorerUrlForAddress + priceAa"
-              >{{ priceAa }}</a
-            >
-          </div>
-          <div>
-            <div class="mt-1">Target price: ${{ reservePriceValue }}</div>
-            <div class="mt-1">Price: ${{ price }}</div>
-          </div>
-        </div>
+      <div class="font-medium text-sm mb-2">
+        Price:
+        <span class="font-light text-sm"> ${{ price }} </span>
       </div>
     </div>
   </div>
